db3f12b3d8687e66568308c4a1c006445a3f3f1c
Tomask0 <tomas.grosek22@gmail.com>
2025-03-28 08:51:12 +0100

    dfg

--- 4.py
+++ 4.py
@@ -3,6 +3,10 @@ class Time:
         self.hours = hours   
         self.minutes = minutes
 
+    def convertMinutes(self):
+        self.hours += self.minutes // 60
+        self.minutes = self.minutes % 60
+    
     def display(self):
         print(self.hours, self.minutes)   
     
@@ -12,20 +16,9 @@ class Time:
     def add(self, newHours, newMinutes):
         self.hours = self.hours + newHours
         self.minutes = self.minutes + newMinutes
-
-        if self.minutes > 59:
-            self.hours += self.minutes//60
-            self.minutes = self.minutes - (60*(self.minutes//60))
-
-        if self.minutes < 0:
-            self.minutes *= -1 
-
-            self.hours -= (self.minutes//60)
-            self.minutes = 60 - (self.minutes%60) 
-            
-            self.display()
+        cas.convertMinutes() 
 
 cas = Time(10, 00)
 cas.display() 
-cas.add(0, -60) 
-#cas.display() 
+cas.add(2, -80)  
+cas.display() 
